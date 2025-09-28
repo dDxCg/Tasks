35be73f82d8d7fa9c9f86652aa98e467d38a2fc4
Cuong_Do <mamxanh5555@gmail.com>
2025-09-28 16:50:30 +0700

    versioning models

---
+++ src/models/v1/model.ts
@@ -0,0 +1,9 @@
+export interface Task {
+  id: number; // Prisma Int with autoincrement
+  title: string;
+  description?: string; // optional
+  status: "TODO" | "IN_PROGRESS" | "DONE";
+  userId: string; // assigned user (can also make optional if unassigned)
+  startDate?: string; // ISO string, optional
+  endDate?: string; // ISO string, optional
+}
